11f38b485b2e1f09632024ad75929641f21f79b6
Klaus Dimde <klaus@dimde.info>
2023-02-28 08:37:19 +0100

    Moved ajax controller and routes to collection

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -14,10 +14,3 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/css/sunhill.css', 'Sunhill\Visual\Controllers\SystemController@css');
 Route::get('/js/sunhill.js', 'Sunhill\Visual\Controllers\SystemController@js');
-
-Route::get('/ajax/searchTags/{class?}', 'Sunhill\Visual\Controllers\AjaxController@searchTags');
-Route::get('/ajax/searchObjects/{class}/{field}', 'Sunhill\Visual\Controllers\AjaxController@searchObjects');
-Route::get('/ajax/searchArrayOfString/{class}/{field}', 'Sunhill\Visual\Controllers\AjaxController@getArrayOfStringSuggestion');
-
-//Route::get('/Computer', 'Sunhill\Visual\Controllers\ComputerController@index');
-Route::post('/Computer/Database/Import/ExecImportPersons', 'Sunhill\Visual\Controllers\ComputerController@ExecImportPersons');
