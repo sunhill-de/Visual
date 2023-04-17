9cfacdce230a5a6b11553ee4756af892b12d7e29
Klaus Dimde <klaus@dimde.info>
2023-04-17 21:16:38 +0200

    Implemented accessData()

---
+++ src/Response/AccessData.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Sunhill\Visual\Response;
+
+use Sunhill\Visual\Facades\Dialogs;
+
+trait AccessData
+{
+   
+    protected function getArrayData($data, $item)
+    {
+        if (isset($data[$item])) {
+            return $data[$item];
+        }
+        if (is_string($item) && (strpos($item,'=>'))) {
+            list($object,$key) = explode('=>',$item);
+            if (isset($data[$object])) {
+                return $this->accessData($data[$object],$key);
+            }
+        }
+        return null;
+    }
+    
+    protected function getObjectData($data, $item)
+    {
+        try {
+            if (is_string($item) && (strpos($item,'=>'))) {
+                list($object,$key) = explode('=>',$item);
+                $object = $data->$object;
+                if (isset($object)) {
+                    return $this->accessData($object,$key);
+                } else {
+                    return '';
+                }
+            }
+            return $data->$item;
+        } catch (\Exception $e) {
+        }
+        return null;        
+    }
+    
+    protected function accessData($data, $item = null)
+    {
+        switch ($item) {
+            case 'keyfield':
+                return Dialogs::getObjectKeyfield($data);
+            case 'id':
+                return $data->getID();
+            case 'class':
+                return $data::getInfo('name');
+        }
+        if (is_array($data)) {
+            return $this->getArrayData($data, $item);
+        }
+        if (is_object($data)) {
+            return $this->getObjectData($data, $item);
+        }
+        if (is_callable($item)) {
+            return $item($data);
+        }
+    }
+    
+}
