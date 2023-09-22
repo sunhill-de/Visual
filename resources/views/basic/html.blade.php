df962200461d811072a99b04cef91e5b11810ab3
Klaus Dimde <klaus@dimde.info>
2023-09-22 09:56:32 +0200

    Implemented iconset

--- resources/views/basic/html.blade.php
+++ resources/views/basic/html.blade.php
@@ -7,6 +7,7 @@
         <title>{{ $sitename }} - @yield('title')</title>
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/sunhill.css') }}">  
+		<script src="https://kit.fontawesome.com/5b0c76269d.js" crossorigin="anonymous"></script>
 		@stack('css')
 		@stack('js')
     </head>
