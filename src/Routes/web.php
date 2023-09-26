7716774fc8cfbb729e98b2e926313fb58aad06b7
Klaus Dimde <klaus@dimde.info>
2023-09-26 23:01:26 +0200

    Added testing for actions

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -17,6 +17,8 @@ Route::get('/js/sunhill.js', 'Sunhill\Visual\Controllers\SystemController@js');
 Route::get('/ajax/{topic}/{additional1?}/{additional2?}', 'Sunhill\Visual\Controllers\AjaxController@ajax');
 
 if (App::environment(['local','staging'])) {
-    Route::get('/dialog', 'Sunhill\Visual\Controllers\TestController@dialog')->name('dialog');
-    Route::post('/dialog/execute', 'Sunhill\Visual\Controllers\TestController@execute')->name('execute');
+    Route::get('/dialog/add', 'Sunhill\Visual\Controllers\TestController@add')->name('test.add');
+    Route::post('/dialog/execadd', 'Sunhill\Visual\Controllers\TestController@execadd')->name('test.execadd');
+    Route::get('/dialog/edit', 'Sunhill\Visual\Controllers\TestController@edit')->name('test.edit');
+    Route::get('/dialog/execadd', 'Sunhill\Visual\Controllers\TestController@execedit')->name('test.execedit');
 }
