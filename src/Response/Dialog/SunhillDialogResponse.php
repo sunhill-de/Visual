e0ddb859792ad1d9c3e29daee4f69334eb45c9b5
Klaus Dimde <klaus@dimde.info>
2023-09-26 18:12:07 +0200

    Switched to unified dialog handling

--- src/Response/Dialog/SunhillDialogResponse.php
+++ src/Response/Dialog/SunhillDialogResponse.php
@@ -12,18 +12,24 @@ use Sunhill\Visual\Response\SunhillBladeResponse;
 abstract class SunhillDialogResponse extends SunhillBladeResponse
 {
 
+    protected $mode = 'add';
+    
     protected $route = '';
     
     protected $route_parameters = [];
     
     protected $method = 'post';
     
-    protected $template = 'visual::basic.dialog';
-    
-    public function setRoute(string $route, array $parameters): SunhillDialog
+    public function setRoute(string $route, array $parameters): SunhillDialogResponse
     {
         $this->route = $route;
         $this->route_parameters = $parameters;
+        return $this;
+    }
+    
+    public function setMode(string $mode): SunhillDialogResponse
+    {
+        $this->mode = $mode;
         return $this;
     }
     
@@ -35,6 +41,7 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
     }
         
     abstract protected function defineDialog(DialogDescriptor $descriptor);
+    abstract protected function execAdd($parameters);
     
     protected function handleDialog()
     {
@@ -51,10 +58,51 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
         $this->params['elements'] = $entries;
     }
     
+    protected function addResponse()
+    {
+        $this->setTemplate('visual::basic.dialog');
+        $this->handleFormParameters();
+        $this->handleDialog();        
+    }
+    
+    protected function parseInput()
+    {
+        $descriptor = new DialogDescriptor();
+        $this->defineDialog($descriptor);
+        $result = [];
+        foreach ($descriptor as $entry) {
+            if (request()->has($entry->getDialogName()) && !empty(request()->input($entry->getDialogName()))) {
+                $result[$entry->getName()] = request()->input($entry->getDialogName());
+            } else {
+                if ($entry->getRequired()) {
+                    $this->inputError($entry, __('This field is required.'));
+                }
+                $result[$entry->getName()] = $entry->getEmptyValue();                
+            }
+        }
+        return $result;
+    }
+    
+    protected function inputError($entry, string $message)
+    {
+        
+    }
+    
+    protected function execAddResponse()
+    {
+        $this->execAdd($this->parseInput());        
+    }
+    
     protected function prepareResponse()
     {
         parent::prepareResponse();
-        $this->handleFormParameters();
-        $this->handleDialog();
+        switch ($this->mode) {
+            case 'add':
+                $this->addResponse();
+                break;
+            case 'execadd':
+                $this->execAddResponse();
+                break;
+        }        
     }
 }
