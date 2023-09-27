8a59be89f3df4d1815ec3ff45e974108897fcf92
Klaus Dimde <klaus@dimde.info>
2023-09-27 12:49:36 +0200

    Cleaned code and implemented re-edit in case of error

--- src/Response/Dialog/SunhillDialogResponse.php
+++ src/Response/Dialog/SunhillDialogResponse.php
@@ -22,6 +22,10 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
     
     protected $error = [];
     
+    protected $recall = '';
+    
+    protected $values;
+    
     public function setRoute(string $route_base, array $parameters): SunhillDialogResponse
     {
         $this->route_base       = $route;
@@ -41,78 +45,12 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
     abstract protected function getEditValues();
     abstract protected function execEdit($parameters);
     
-    protected function handleAddFormParameters()
-    {
-        $this->params['dialog_method'] = $this->method;
-        $this->params['dialog_route'] = $this->route_base.'.execadd';
-        $this->params['dialog_route_parameters'] = $this->route_parameters;
-    }
-    protected function handleAddDialog()
-    {
-        $descriptor = new DialogDescriptor();
-        $this->defineDialog($descriptor);
-        $entries = [];
-        foreach ($descriptor as $entry) {
-            $element = new \StdClass();
-            $element->label = $entry->getLabel();
-            $element->name = $entry->getName();
-            $element->dialog = $entry->getHTMLCode();
-            $entries[] = $element;
-        }
-        $this->params['elements'] = $entries;
-    }
-    
-    protected function addResponse()
+    protected function fillFormTemplate(string $route_addition, array $values = [])
     {
         $this->setTemplate('visual::basic.dialog');
-        $this->handleAddFormParameters();
-        $this->handleAddDialog();        
-    }
-    
-    protected function parseInput()
-    {
-        $descriptor = new DialogDescriptor();
-        $this->defineDialog($descriptor);
-        $result = [];
-        foreach ($descriptor as $entry) {
-            if (request()->has($entry->getDialogName()) && !empty(request()->input($entry->getDialogName()))) {
-                $result[$entry->getName()] = request()->input($entry->getDialogName());
-            } else {
-                if ($entry->getRequired()) {
-                    $this->inputError($entry, __('This field is required.'));
-                }
-                $result[$entry->getName()] = $entry->getEmptyValue();                
-            }
-        }
-        return $result;
-    }
-    
-    protected function inputError($entry, string $message)
-    {
-        $this->error[$entry->getName()] = $message;
-    }
-    
-    protected function execAddResponse()
-    {
-        $this->execAdd($this->parseInput());        
-    }
-    
-    protected function editResponse()
-    {
-        
-    }
-    
-    protected function execEditResponse()
-    {
-        
-    }
-    
-    protected function handleError()
-    {
         $this->params['dialog_method'] = $this->method;
-        $this->params['dialog_route'] = $this->route_base.'.execadd';
+        $this->params['dialog_route'] = $this->route_base.'.'.$route_addition;
         $this->params['dialog_route_parameters'] = $this->route_parameters;
-        $this->setTemplate('visual::basic.dialog');
         
         $descriptor = new DialogDescriptor();
         $this->defineDialog($descriptor);
@@ -124,13 +62,80 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
             if (array_key_exists($element->name,$this->error)) {
                 $element->error = $this->error[$element->name];
             }
-            $element->value = $entry->getValue(request()->input($entry->getName()));
-            $entry->value($element->value);
+            if (array_key_exists($element->name, $values)) {
+                $entry->loadValue($values);                
+            }
             $element->dialog = $entry->getHTMLCode();
             $entries[] = $element;
         }
         $this->params['elements'] = $entries;
+    }
+    
+    protected function addResponse()
+    {
+        $this->fillFormTemplate('execadd');
+    }
+    
+    protected function parseInput()
+    {
+        $descriptor = new DialogDescriptor();
+        $this->defineDialog($descriptor);
+        $result = [];
+        foreach ($descriptor as $entry) {
+            if (request()->has($entry->getDialogName()) && !empty(request()->input($entry->getDialogName()))) {
+                switch ($entry::class) {
+                    case DialogEntryList::class:
+                        $result[$entry->getName()] = request()->input($entry->getDialogName());
+                        $result['name_'.$entry->getName()] = request()->input('name_'.$entry->getDialogName());
+                        break;
+                    case DialogEntryInputLookup::class:
+                        $result['input_'.$entry->getName()] = request()->input('input_'.$entry->getName());
+                        $result['value_'.$entry->getName()] = request()->input('value_'.$entry->getName());
+                    default:    
+                        $result[$entry->getName()] = request()->input($entry->getDialogName());
+                }
+            } else {
+                if ($entry->getRequired()) {
+                    $this->inputError($entry, __('This field is required.'));
+                }
+                $result[$entry->getName()] = $entry->getEmptyValue();                
+            }
+        }
+        $this->input = $result;
+        return $result;
+    }
+    
+    protected function inputError($entry, string $message)
+    {
+        $this->error[$entry->getName()] = $message;
+    }
+    
+    protected function execAddResponse()
+    {
+        $this->recall = 'execadd';
+        $input = $this->parseInput();
+        if ($this->error) {
+            return;
+        }
+        if ($this->error = $this->execAdd($input)) {
+            return;
+        }        
+    }
+    
+    protected function editResponse()
+    {
         
+    }
+    
+    protected function execEditResponse()
+    {
+        $this->recall = 'execedit';
+        
+    }
+    
+    protected function handleError($values = [])
+    {
+        $this->fillFormTemplate($this->recall,$values);        
     }
     
     protected function prepareResponse()
@@ -151,7 +156,7 @@ abstract class SunhillDialogResponse extends SunhillBladeResponse
                 break;
         }
         if (!empty($this->error)) {
-            $this->handleError();
+            $this->handleError($this->input);
         }
     }
 }
